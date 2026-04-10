c2a384cbaca92dd56f2f2195c35dce375ee886b5
danieldiamand <51838675+danieldiamand@users.noreply.github.com>
2026-04-10 11:50:04 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: 'https://github.com/danieldiamand/countries_of_the_world/',
   build: {
     outDir: 'dist',
     sourcemap: false,
